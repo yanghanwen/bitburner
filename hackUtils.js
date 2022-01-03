02d06dc401f27f089513efef785c086d30913ca1
yanghanwen <775896071@qq.com>
2022-01-03 22:36:09 +0800

    Update hackUtils.js

--- hackUtils.js
+++ hackUtils.js
@@ -1,11 +1,15 @@
 var utils = ["NUKE.exe","BruteSSH.exe","FTPCrack.exe","relaySMTP.exe","HTTPWorm.exe","SQLInject.exe"];
 
-/** @param {NS} ns **/
 export async function main(ns) { 
+
 }
 
-//尝试对目标使用所有程序
-export function runall(ns , host)
+/** 
+ * 尝试对目标使用所有程序
+ * @param {NS} ns 
+ * @param host 目标主机 
+ **/
+export function runAll(ns , host)
 {
 	ns.tprintf("尝试使用所有程序 at %s",host);
 	for(var i in utils)
@@ -14,6 +18,12 @@ export function runall(ns , host)
 	}
 }
 
+/** 
+ * 尝试对目标使用指定程序
+ * @param {NS} ns 
+ * @param file 指定程序 
+ * @param host 目标主机 
+ **/
 export function run(ns , file , host)
 { 
 	var flag = true;
@@ -66,4 +76,36 @@ export function run(ns , file , host)
 	{
 		ns.tprintf("%s 不存在、或者未安装 指定文件 %s",host,file);  
 	}
+}
+
+/** 
+ * 获得预计吃满内存需要的线程数
+ * @param {NS} ns 
+ * @param forecast 预估RAM消耗
+ * @param host 目标主机 
+ **/
+export function getThread(ns,forecast,host)
+{
+	var maxRam = ns.getServerMaxRam();
+	var thread = Math.floor(maxRam/forecast);
+	return thread;
+}
+
+/** 
+ * 获得在目标服务器上运行指定脚本，吃满内存需要的线程数
+ * @param {NS} ns 
+ * @param script 脚本名，需要在目标主机上
+ * @param host 目标主机 
+ **/
+export function getScriptThread(ns,script,host)
+{
+	var ram = ns.getScriptRam(script);
+	if(ram==0)
+	{
+		tprintf("目标主机：%s 不存在脚本：%s",host,script);
+		return 1;
+	}
+	var maxRam = ns.getServerMaxRam();
+	var thread = Math.floor(maxRam/forecast);
+	return thread;
 }
