cb0964bb6fb96ac1b05089ac98b594176fbc5d31
yanghanwen <775896071@qq.com>
2022-01-03 19:47:59 +0800

    Update initServers.js

--- initServers.js
+++ initServers.js
@@ -1,14 +1,13 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-    var script = ns.args[0]; 
-    var target = ns.args[1];
+    //自动购买服务器 
+    var ram = ns.args[0];//核心数量
 
-    var ram = 8;
     var i = 0;
     var limit = ns.getPurchasedServerLimit(); 
     while(i<limit)
     {
-        await ns.sleep(100);
+        await ns.sleep(10);
         if(ns.getServerMoneyAvailable('home')>ns.getPurchasedServerCost(ram))
         {
             var name = "pserv-" + i;
@@ -19,12 +18,7 @@ export async function main(ns) {
             }
             else
             {
-                ns.tprintf("购买%s成功",name);     
-
-                await ns.scp(script,'home',host); 
-                ns.tprintf("拷贝 %s 到 %s ",script,host);     
- 
-                await ns.exec(script,host,2,target); 
+                ns.tprintf("购买%s成功",name);      
             }
             i = i+1;
         }
